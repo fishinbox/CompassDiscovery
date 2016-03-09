61914fc907f8980b0b7f19b4c102e673f62290db
Zirui Zhuang <zr.zz.alp@gmail.com>
2016-03-09 09:53:57 +0800

    check cobblerd service status

--- utils/config_cobbler.sh
+++ utils/config_cobbler.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # TODO configure cobbler signature
+# TODO Check SELinux status
+# TODO Check cobblerd service
 # TODO Check distro status
 # TODO Check system status
 # TODO Update distro
