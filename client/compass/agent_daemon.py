136ea83f937005e340775ccddba1cc7781c51827
Zirui Zhuang <zr.zz.alp@gmail.com>
2016-04-01 10:04:10 +0800

    add logging

--- client/compass/agent_daemon.py
+++ client/compass/agent_daemon.py
@@ -1,3 +1,18 @@
+# Copyright 2016 Network Intelligence Research Center, 
+# Beijing University of Posts and Telecommunications
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # To kick off the script, run the following from the python directory:
 #   PYTHONPATH=`pwd` python testdaemon.py start
 
@@ -8,19 +23,24 @@ import socket
 
 #third party libs
 from daemon import runner
-from service_listener import get_server_info
+
 
 class App():
     
     def __init__(self):
         self.stdin_path = '/dev/null'
-        self.stdout_path = '/dev/null'
-        self.stderr_path = '/dev/null'
+        self.stdout_path = '/tmp/agent_stdout'
+        self.stderr_path = '/tmp/agent_stderr'
         self.pidfile_path =  '/tmp/agent_daemon.pid'
-        self.pidfile_timeout = 5   
+        self.pidfile_timeout = 5
 
     def run(self):
+        from service_listener import get_server_info
+        from common import Log
+
+        Log.debug('enter run')
         address, port, nics = get_server_info()
+        Log.debug((address, port, nics))
         clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         clientsocket.connect((address, port))
         clientsocket.send(str(nics))
--- client/compass/service_listener.py
+++ client/compass/service_listener.py
@@ -1,8 +1,24 @@
+# Copyright 2016 Network Intelligence Research Center, 
+# Beijing University of Posts and Telecommunications
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from zeroconf import ServiceBrowser, Zeroconf
 import time
 import socket 
 import netifaces
 
+from common import *
 
 class ServiceListener(object):
     service_names = []
@@ -15,14 +31,18 @@ class ServiceListener(object):
  
 
 def get_server_info():
+    Log.debug('get_service_info')
     zeroconf = Zeroconf()
     listener = ServiceListener()
-    service_type = "_compass_discovery1._tcp.local."
+    service_type = "_compass_discovery._tcp.local."
+    Log.debug('enter browser')
     browser = ServiceBrowser(zeroconf, service_type, listener)
-
+    
     try:
+        Log.debug('enter try')
         while len(listener.service_names)<=0:
             pass
+        Log.debug('tried')
         name = listener.service_names[0]
         service = zeroconf.get_service_info(service_type, name)
         address = socket.inet_ntoa(service.address)
