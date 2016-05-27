0b3be7dfbbe50ce4a50d8c5ca43a878abdbfca02
Zirui Zhuang <zr.zz.alp@gmail.com>
2016-05-27 18:13:10 +0800

    fix processor threads and product detection

--- client/compass/lshw.py
+++ client/compass/lshw.py
@@ -106,35 +106,46 @@ def getNetwork(inventory):
 
 
 def getProcessor(inventory):
-	find_cpus = etree.XPath(".//node[@class='processor']")
+    find_cpus = etree.XPath(".//node[@class='processor']")
 
-	count = 0
-	total_cores = 0
-	total_enabledcores = 0
-	total_threads = 0
+    count = 0
+    total_cores = 0
+    total_enabledcores = 0
+    total_threads = 0
 
-	devices =[]
+    devices =[]
 
-	for i in  find_cpus(inventory):
-		if i.find('size') is not None:
-			count = count + 1
-			product = i.find('product').text
-			# print "width: " + i.find('width').text
-			cores = i.find('configuration/setting/[@id="cores"]').get('value')
-			enabledcores = i.find('configuration/setting/[@id="enabledcores"]').get('value')
-			threads = i.find('configuration/setting/[@id="threads"]').get('value')
+    for i in  find_cpus(inventory):
+        if i.find('size') is not None and not bool(i.get('disabled', False)):
+            #pprint(etree.tostring(i))
+            pprint(etree.tostring(i.find('size')))
+            count = count + 1
+            product = i.find('product').text
+            # print "width: " + i.find('width').text
+            #pprint(etree.tostring(i.find('configuration')))
+            cores = i.find('configuration/setting/[@id="cores"]').get('value')
+            enabledcores = i.find('configuration/setting/[@id="enabledcores"]').get('value')
+            try:
+                threads = i.find('configuration/setting/[@id="threads"]').get('value')
+            except:
+                threads = 1
 
-			total_cores = total_cores + int(cores)
-			total_enabledcores = total_enabledcores + int(enabledcores)
-			total_threads = total_threads + int(threads)
-			devices.append({'product':product, 'cores':cores, 'enabledcores': enabledcores, 'threads': threads})
+            total_cores = total_cores + int(cores)
+            total_enabledcores = total_enabledcores + int(enabledcores)
+            total_threads = total_threads + int(threads)
+            devices.append({
+                'product': product,
+                'cores': cores,
+                'enabledcores': enabledcores,
+                'threads': threads
+            })
 
     return {
-            'count':count,
-            'total_cores':total_cores,
-            'total_enabledcores': total_enabledcores,
-    		'total_threads':total_threads,
-            'devices':devices
+        'count': count,
+        'total_cores': total_cores,
+        'total_enabledcores': total_enabledcores,
+        'total_threads': total_threads,
+        'devices': devices
     }
 
 
@@ -147,6 +158,4 @@ def getMachineInfo():
         'disk': getDisk(lshw),
         'network': getNetwork(lshw),
     }
-
-
 
